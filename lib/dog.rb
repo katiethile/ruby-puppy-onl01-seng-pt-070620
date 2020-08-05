fd6a3ef39ee120e19ab9a07c12f5888801139750
Katie Le <lethikatie@gmail.com>
2020-08-05 20:10:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,26 @@
 class Dog 
-  
+  attr_accessor :name 
   @@all = []
-  
-attr_accessor :name
-
   def initialize(name)
     @name = name
     self.save
-end 
-
-  def self.all 
-    @@all
-end
-
-  def self.print_all 
-    @@all.each do |dog|
-      puts dog.name
   end 
-end 
-
+  
+  def self.all
+    @@all
+  end
+  
   def self.clear_all
     @@all.clear
-end 
-
+  end
+  
+  def self.print_all
+    @@all.each do |dog|
+      puts dog.name
+   end 
+  end 
+  
   def save 
-    @@all << self 
+    @@all << self
   end 
 end 
